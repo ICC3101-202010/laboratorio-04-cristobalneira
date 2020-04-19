3587a4e61b9f43339ea8d8221d6db64e0cdc2f77
Cristobal <c@iMac-de-Rene.local>
2020-04-19 19:50:47 -0400

    First commit

---
+++ Proyecto4Neira/Proyecto4Neira/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto4Neira
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
